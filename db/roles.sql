22ff984057765983ab1e4c6a29f8bc4fabcaf69a
Jose Calvo <jacalvoaguilar@gmail.com>
2026-02-05 22:51:00 -0600

    correccion en los roles

--- db/roles.sql
+++ db/roles.sql
@@ -1,13 +1,18 @@
-CREATE ROLE app_user WITH LOGIN PASSWORD 'cafeteria_secure_pass';
+-- Crear rol de aplicación (solo lectura sobre vistas)
+CREATE ROLE app WITH LOGIN PASSWORD 'cafeteria_secure_pass';
 
 -- Otorgar permiso de conexión
-GRANT CONNECT ON DATABASE cafeteria_up TO app_user;
-GRANT USAGE ON SCHEMA public TO app_user;
+GRANT CONNECT ON DATABASE cafeteria_up TO app;
+GRANT USAGE ON SCHEMA public TO app;
 
 -- Permiso ÚNICAMENTE sobre las vistas (Prohibido sobre tablas base)
-GRANT SELECT ON vw_sales_daily TO app_user;
-GRANT SELECT ON vw_top_products_ranked TO app_user;
-GRANT SELECT ON vw_inventory_risk TO app_user;
-GRANT SELECT ON vw_customer_value TO app_user;
-GRANT SELECT ON vw_payment_mix TO app_user;
-GRANT SELECT ON vw_channel_performance TO app_user;
+GRANT SELECT ON vw_sales_daily TO app;
+GRANT SELECT ON vw_top_products_ranked TO app;
+GRANT SELECT ON vw_inventory_risk TO app;
+GRANT SELECT ON vw_customer_value TO app;
+GRANT SELECT ON vw_payment_mix TO app;
+GRANT SELECT ON vw_channel_performance TO app;
+
+-- Verificación de seguridad (ejecutar manualmente):
+-- SELECT * FROM products; -- Debe fallar con usuario app
+-- SELECT * FROM vw_sales_daily; -- Debe funcionar con usuario app
